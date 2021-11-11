3e3a5f35643a3c06d3b0861753937c3d9f7a4093
yujunjiao <yujunjiao@jd.com>
2021-11-11 14:22:51 +0800

    1

---
+++ index2.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Author: yujj
+ * @Date: 2021-11-11 14:23:10
+ * @LastEditTime: 2021-11-11 14:23:10
+ * @LastEditors: yujj
+ */
+console.log(2);
---
+++ index4.js
@@ -0,0 +1,8 @@
+/*
+ * @Description: 
+ * @Author: yujj
+ * @Date: 2021-11-11 14:24:02
+ * @LastEditTime: 2021-11-11 14:24:03
+ * @LastEditors: yujj
+ */
+console.log(4);
